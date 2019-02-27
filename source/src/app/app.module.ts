98c09c7b4763f0c23a95462762965ef9d36d183a
rubykohnsite <rubykohnsite@gmail.com>
2019-02-27 18:47:15 -0500

    component

--- source/src/app/app.module.ts
+++ source/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    ContactComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
